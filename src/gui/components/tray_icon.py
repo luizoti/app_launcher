1f407308f00596d808596bdf7afe667718934ef9
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2025-07-27 00:14:08 -0300

    ref: Refactored to reduce signals and usage of parent

--- src/gui/components/tray_icon.py
+++ src/gui/components/tray_icon.py
@@ -1,20 +1,18 @@
-import traceback
-from PyQt5.QtCore import pyqtSignal, QThread
+from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QSystemTrayIcon
 
+from src.enums import actions_map_reversed
 from src.gui.components.context_menu import ContextMenu
 from src.utils import base64_to_icon
 
 
 # Classe para o ícone do tray
 class TrayIcon(QSystemTrayIcon):
-    hide_signal = pyqtSignal()
-    show_signal = pyqtSignal()
+    tray_action = pyqtSignal(int)
 
     def __init__(self, parent=None, icon_base64: str = None):
         super(TrayIcon, self).__init__(parent=parent)
-        self.parent = parent
-        self.menu = ContextMenu(parent=parent)
+        self.menu = ContextMenu()
 
         if icon_base64:
             self.setIcon(base64_to_icon(icon_base64))
@@ -24,15 +22,18 @@ class TrayIcon(QSystemTrayIcon):
         self.setVisible(True)
 
     def _set_signals(self):
-        self.hide_signal.connect(self.parent.hide)
-        self.show_signal.connect(self.parent.show)
-
         self.activated.connect(self._handle_tray_click)
+
+        self.menu.change_visibility_action.triggered.connect(
+            lambda x: self.tray_action.emit(actions_map_reversed.get("toggle_view"))
+        )
+        self.menu.exit_action.triggered.connect(
+            lambda x: self.tray_action.emit(actions_map_reversed.get("close"))
+        )
 
     def _handle_tray_click(self, reason):
         if reason == self.Trigger:
-            signal = self.hide_signal if self.parent.isVisible() else self.show_signal
-            signal.emit()
+            self.tray_action.emit(actions_map_reversed.get("toggle_view"))
 
     # TODO: CHANGE COLOR IF DEVICE IS CONNECTED WITH INDIVIDUAL DEVICE CONTROL
     # def tray_icon_controller():
@@ -40,18 +41,3 @@ class TrayIcon(QSystemTrayIcon):
     #         if device.get("tray") and device.get("connected"):
     #             return True
     #     return False
-
-    def exit(self):
-        """Fecha o aplicativo."""
-        try:
-            QThread().quit()
-            # self.parent.thread.quit()
-            # self.thread.quit()  # Para a thread de forma segura
-            # self.thread.wait()  # Aguarda que a thread termine
-            # self.main_app.quit()  # Fecha o aplicativo
-            # self.destroy()
-            return True
-        except:  # noqa
-            pass
-        print(f"Exit error: {traceback.format_exc()}")
-        return None
