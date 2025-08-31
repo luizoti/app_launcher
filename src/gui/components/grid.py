276b7dea9738537d2b1bb4d3d4f0354380ac321e
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2025-08-30 21:39:56 -0300

    ref: Simplified app grid calc

--- src/gui/components/grid.py
+++ src/gui/components/grid.py
@@ -1,6 +1,7 @@
+import more_itertools
 from PyQt5.QtWidgets import QGridLayout
 
-from src.gui.app import CommandExecutor
+from src.command_executor import CommandExecutor
 from src.gui.components.custom_button import CustomButton
 
 
@@ -8,35 +9,39 @@ class AppGrid(QGridLayout):
     def __init__(self, row_limit: int = None):
         super(AppGrid, self).__init__()
         self.row_limit = row_limit
+
+        self.mapped_grid = {}
+
         if not self.row_limit:
             print("ERRO - No row limit")
             raise AttributeError
 
-    def remove_from_grid(self, app_name=None):
+    def __remove_from_grid(self, app_name=None):
         if not app_name:
-            raise TypeError("app_name cannot be null")
+            raise TypeError("Argument `app_name` cannot be null")
         index, app_widget = self.__search_app_on_grid(app_name=app_name)
         widget = app_widget.widget()
         if widget is not None:
             self.removeWidget(widget)
             widget.deleteLater()
-        self._reorganize_grid()
+        # self.__reorganize_grid()
+        self._plot_app_grid()
 
-    def _reorganize_grid(self):
-        widgets = []
-        for i in reversed(range(self.count())):
-            item = self.takeAt(i)
-            widget = item.widget()
-            if widget:
-                widgets.append(widget)
-        row = 0
-        col = 0
-        for widget in reversed(widgets):
-            self.addWidget(widget, row, col)
-            col += 1
-            if col >= self.row_limit:
-                col = 0
-                row += 1
+    # def __reorganize_grid(self):
+    #     widgets = []
+    #     for i in reversed(range(self.count())):
+    #         item = self.takeAt(i)
+    #         widget = item.widget()
+    #         if widget:
+    #             widgets.append(widget)
+    #     row = 0
+    #     col = 0
+    #     for widget in reversed(widgets):
+    #         self.addWidget(widget, row, col)
+    #         col += 1
+    #         if col >= self.row_limit:
+    #             col = 0
+    #             row += 1
 
     def __search_app_on_grid(self, app_name):
         for index in range(0, 1000):
@@ -48,24 +53,29 @@ class AppGrid(QGridLayout):
                 raise err
         return None
 
-    def render_app_grid(self, apps: dict = None):
-        if not apps:
-            print("No apps from argument - apps")
-        row = 0
-        row_app_counter = 0
+    @staticmethod
+    def __button_generator(app_name, app_data):
+        """Create a button with command based on app name and app data."""
+        return CustomButton(
+            icon_base64=app_data.get("icon"),
+            on_click=CommandExecutor(command=app_data.get("cmd", [])).execute,
+            name=app_name
+        )
 
-        for app_name, app_settings in apps.items():
-            if row_app_counter == self.row_limit:
-                row += 1
-                row_app_counter = 0
-            executor = CommandExecutor(command=app_settings.get("cmd", []))
-            button = CustomButton(
-                icon_base64=app_settings.get("icon"),
-                on_click=executor.execute,
-                name=app_name
-            )
-            self.addWidget(button, row, row_app_counter)
-            row_app_counter += 1
+    def __rebuild_mapped_grid(self, apps: dict = None):
+        if not apps:
+            raise TypeError("Argument `apps` cannot be None")
+        self.mapped_grid = dict(
+            enumerate(more_itertools.batched([self.__button_generator(*x) for x in apps.items()], self.row_limit))
+        )
+
+    def _plot_app_grid(self, apps: dict = None):
+        if not apps:
+            raise TypeError("Argument `apps` cannot be None")
+        self.__rebuild_mapped_grid(apps=apps)
+        for row_index, apps in self.mapped_grid.items():
+            for app_index, app in enumerate(apps):
+                self.addWidget(app, row_index, app_index)
         return self
 
     def change_focus(self, app_name):
