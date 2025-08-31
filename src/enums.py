38a404b4cb843be7a2ae706514d37a049a12f819
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2025-08-30 22:22:13 -0300

    feat: add a mapping to actions

---
+++ src/enums.py
@@ -0,0 +1,14 @@
+actions_map = {
+    1: "up",
+    2: "down",
+    3: "left",
+    4: "right",
+    5: "enter",
+    6: "options",
+    7: "toggle_view",
+    8: "close"
+}
+
+actions_map_reversed = {
+    y: x for x, y in actions_map.items()
+}
