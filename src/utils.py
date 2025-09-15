275e4fc5bf7eb690ea0a0d4c032611bcd27917f4
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2025-09-14 21:36:05 -0300

    ref: refactory base64_to_icon to build_icon to work with base64 strings and real file paths

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,25 @@
 import base64
+from pathlib import Path
 
 import psutil
-from PyQt5.QtGui import QIcon, QImage, QPixmap
+from PyQt5.QtGui import QIcon, QPixmap, QImage
+
+from src.settings import SettingsManager
 
 
-def base64_to_icon(icon=None, connected=False) -> QIcon:
-    image = QImage()
-    image.loadFromData(base64.b64decode(icon))
-    return QIcon(QPixmap.fromImage(image))
+def build_icon(icon=None, settings=SettingsManager().get_settings()) -> QIcon | QIcon | None:
+    try:
+        if Path(icon).exists():
+            return QIcon(icon)
+
+        local_icon: Path = settings.get("icons_directory").joinpath(icon)
+
+        if local_icon.exists():
+            return QIcon(str(local_icon))
+    except (OSError, TypeError):
+        image = QImage()
+        image.loadFromData(base64.b64decode(icon))
+        return QIcon(QPixmap.fromImage(image))
 
 
 def check_running_processes(search_process=None):
