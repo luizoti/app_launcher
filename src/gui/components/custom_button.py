2aca301a60524f5ddeed583064d32cfe33ea0e24
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2026-01-04 23:14:03 -0300

    feat: New color and resize animation

--- src/gui/components/custom_button.py
+++ src/gui/components/custom_button.py
@@ -1,70 +1,91 @@
-from PyQt5.QtCore import QSize, Qt, pyqtSignal, QEvent
-from PyQt5.QtGui import QEnterEvent, QDragLeaveEvent
-from PyQt5.QtWidgets import QPushButton
-
+from PyQt5.QtCore import QSize, Qt, pyqtSignal
+from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import QPushButton, QGraphicsColorizeEffect
 from src.utils import build_icon
+
+ICO_PADDING = 16
 
 
 class CustomButton(QPushButton):
     focused = pyqtSignal(str)
 
-    def __init__(self, label: str = None, icon: str = None, on_click=None, name: str = None):
-        self.name = name
-        if not label:
-            label = ""
-        super(CustomButton, self).__init__(label)
+    def __init__(self, label: str = None, icon: str = None, on_click=None, name: str = None, size=None,
+                 icon_size: QSize = None):
+        self.name = name or ""
+        self.disable_animation = False
+        super(CustomButton, self).__init__(label if label else "")
         self.on_click = on_click
-
-        self.default_color = "#FFFFFF"
+        self.normal_color = "#FFFFFF"
         self.hover_color = "#444444"
         self.text_color = "#333333"
-
-        self.setIconSize(QSize(100, 100))
-        self.setMinimumSize(120, 120)
-        self.setMaximumSize(120, 120)
-
+        self.normal_size = size[0] - ICO_PADDING if size else 112
+        self.focused_size = icon_size.width() + ICO_PADDING if icon_size else 128
+        self.setStyleSheet(self._get_stylesheet(self.normal_color))
+        self.setFixedSize(*(self.normal_size, self.normal_size,))
+        self.setIconSize(QSize(self.focused_size, self.focused_size))
         if icon:
             self.setIcon(build_icon(icon))
-
-        self.setStyleSheet(self._get_stylesheet(background_color=self.default_color))
-
+        self.effect = QGraphicsColorizeEffect(self)
+        self.effect.setColor(QColor(0, 0, 0))
+        self.effect.setStrength(0.8)
+        self.setGraphicsEffect(self.effect)
         self.clicked.connect(self._handle_click)
+        self._update_visuals(False)
+
+    def _get_stylesheet(self, background_color: str):
+        return f"""
+        QPushButton {{
+            font-size: 18px;
+            background-color: {background_color};
+            color: {self.text_color if background_color == self.normal_color else self.normal_color};
+            border: 0px solid #555555;
+            border-radius: 10px;
+            text-align: center;
+        }}
+        QPushButton:focus {{
+            outline: none;
+        }}
+        """
+
+    def no_animation(self):
+        self.disable_animation = True
+
+    def _update_visuals(self, is_active: bool):
+        ico_sizes = (self.focused_size, self.focused_size) if is_active else (self.normal_size, self.normal_size)
+        self.effect.setEnabled(not is_active)
+        self.setStyleSheet(self._get_stylesheet(self.normal_color if is_active else self.hover_color))
+        if self.disable_animation:
+            return None
+        self.set_ico_size(*ico_sizes)
+        self.setMaximumSize(*ico_sizes)
+        if is_active:
+            self.focused.emit(self.name)
+
+    def set_ico_size(self, width: int, height: int):
+        self.setIconSize(QSize(width, height))
 
     def _handle_click(self):
         if self.on_click:
             self.on_click()
 
-    def _get_stylesheet(self, background_color: str):
-        return f"""
-        QPushButton:focus {{
-            background-color: gray;
-            padding: none;
-            outline: none;
-        }}
-        QPushButton {{
-            font-size: 18px;
-            background-color: {background_color};
-            color: {self.text_color};
-            border: 2px solid #555555;
-            border-radius: 15px;
-            padding: 10px;
-            text-align: center;
-        }}
-        """
-
-    def event(self, e):
-        if e.type() == QEvent.KeyPress:
-            self.focused.emit(self.name)
-        return super(CustomButton, self).event(e)
-
-    def enterEvent(self, event: QEnterEvent):
-        self.setStyleSheet(self._get_stylesheet(self.hover_color))
-        self.focused.emit(self.name)
+    def enterEvent(self, event):
+        self._update_visuals(True)
         super().enterEvent(event)
 
-    def leaveEvent(self, event: QDragLeaveEvent):
-        self.setStyleSheet(self._get_stylesheet(self.default_color))
+    def leaveEvent(self, event):
+        self._update_visuals(self.hasFocus())
         super().leaveEvent(event)
 
+    def focusInEvent(self, event):
+        self._update_visuals(True)
+        super().focusInEvent(event)
+
+    def focusOutEvent(self, event):
+        self._update_visuals(self.underMouse())
+        super().focusOutEvent(event)
+
     def keyPressEvent(self, event):
-        self.click() if event.key() == Qt.Key_Return else super().keyPressEvent(event)
+        if event.key() in (Qt.Key_Return, Qt.Key_Enter):
+            self.click()
+        else:
+            super().keyPressEvent(event)
