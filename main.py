5a370d640dec835e6fd2db59c528e3aa17f07113
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2026-02-16 13:58:17 -0300

    feat: Add convert print to log messages

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import os
 import sys
+from logging import getLogger
 
 from PyQt5.QtWidgets import QApplication
 
 from src.gui.app import AppMainWindow
-from src.insancie import read_pid_file, write_pid_file, check_pid_exist, get_current_pid
+from src.instance import (check_pid_exist, get_current_pid, read_pid_file,
+                          write_pid_file)
+
+logger = getLogger(__name__)
+
 
 os.environ.setdefault("QT_QPA_PLATFORM", "xcb")
 
@@ -17,9 +22,9 @@ if __name__ == "__main__":
     loaded_pid = read_pid_file()
     if loaded_pid:
         if check_pid_exist(loaded_pid):
-            print("DEBUG - Another instancie running with PID:", loaded_pid)
+            logger.debug("Another instancie running with PID:", loaded_pid)
             sys.exit(1)
-    print("INFO - Started with PID:", get_current_pid())
+    logger.info("Started with PID:", get_current_pid())
     write_pid_file()
     app_window = AppMainWindow()
     app_window.show_ui()
