aee5361a90d604d444f537b466a9dc3f726e3668
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2026-02-16 20:30:39 -0300

    ref: Apply type hints on context menu

--- src/gui/components/context_menu.py
+++ src/gui/components/context_menu.py
@@ -1,9 +1,11 @@
-from PyQt5.QtWidgets import QMenu
+import typing
+
+from PyQt5.QtWidgets import QMenu, QWidget
 
 
 class ContextMenu(QMenu):
 
-    def __init__(self, parent=None, *args, **kwargs):
+    def __init__(self, parent: typing.Optional[QWidget]=None, *args: typing.Any, **kwargs: typing.Any):
         super(ContextMenu, self).__init__(parent=parent, *args, **kwargs)
-        self.change_visibility_action = self.addAction("Hide/Show")
-        self.exit_action = self.addAction("Exit")
+        self.change_visibility_action = self.addAction("Hide/Show") # type: ignore
+        self.exit_action = self.addAction("Exit") # type: ignore
