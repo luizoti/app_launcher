c36a0e1f28879ec22c6208697fc73ce1ddb64ce7
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2026-02-16 20:28:48 -0300

    ref: Refactored to take best legibility

--- src/gui/action_manager.py
+++ src/gui/action_manager.py
@@ -1,17 +1,23 @@
-from PyQt5 import QtCore
-from PyQt5.QtCore import pyqtSignal
+
+import logging
+
+from PyQt5.QtCore import pyqtSignal, pyqtSlot  # type: ignore
 
 from src.enums import actions_map
+
+logger = logging.getLogger(__name__)
 
 
 class ActionManager:
     actions = pyqtSignal(int)
 
-    @QtCore.pyqtSlot(int, name="action_handler")
-    def action_handler(self, action: int = None):
-        if not action:
-            raise TypeError("Argument `action` cannot be None")
-        try:
-            getattr(self, actions_map.get(action))()
-        except AttributeError:
-            pass
+    @pyqtSlot(int, name="action_handler")
+    def action_handler(self, action_id: int) -> None:
+        method_name = actions_map.get(action_id)
+        if not method_name:
+            return
+        method = getattr(self, method_name, None)
+        if callable(method):
+            method()
+            return
+        logger.error(f"Erro: Método {method_name} não encontrado em {self.__class__.__name__}")
