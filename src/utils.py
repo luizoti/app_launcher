24922bf705851b8eff4a6fa46570153f18cf5d09
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2026-02-16 13:52:56 -0300

    feat: Added a default missing icon

--- src/utils.py
+++ src/utils.py
@@ -1,27 +1,38 @@
 import base64
+import typing
 from pathlib import Path
 
 import psutil
-from PyQt5.QtGui import QIcon, QPixmap, QImage
+from PyQt5.QtGui import QIcon, QImage, QPixmap
 
-from src.settings import SettingsManager
+from src.settings import SettingsManager, SettingsModel
 
 
-def build_icon(icon=None, settings=SettingsManager().get_settings()) -> QIcon | QIcon | None:
-    if icon is None:
-        raise TypeError("Variable `icon` is required, cannot be None")
+def build_icon(
+    icon: typing.Optional[typing.Text] = None, settings: SettingsModel=SettingsManager().get_settings()
+) -> QIcon | QIcon | None:
+    """
+    Load ico from file or base64 and return a QIcon, loads absolute path, filename from icos directory or base64 string.
+    
+    :param icon: base64 or icon name or absolute ico path
+    :type icon: typing.Optional[typing.Text]
+    :param settings: App Settings model
+    :type settings: SettingsModel
+    :return: QIcon based on loaded icon file
+    :rtype: QIcon
+    """
+    local_icon_directory: Path = Path(settings.icons_directory)
+    if not icon:
+        return QIcon(str(local_icon_directory.joinpath("missing.ico")))
     try:
-        if Path(icon).exists():
-            return QIcon(icon)
-
-        local_icon: Path = settings.get("icons_directory").joinpath(icon)
-
-        if local_icon.exists():
-            return QIcon(str(local_icon))
+        if local_icon_directory.joinpath(icon).exists():
+            print("teste", icon)
+            return QIcon(str(local_icon_directory.joinpath(icon)))
     except (OSError, TypeError):
-        image = QImage()
-        image.loadFromData(base64.b64decode(icon))
-        return QIcon(QPixmap.fromImage(image))
+        pass
+    image = QImage()
+    image.loadFromData(base64.b64decode(icon))
+    return QIcon(QPixmap.fromImage(image))
 
 
 def check_running_processes(search_process=None):
