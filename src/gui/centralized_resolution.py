c73f0e27aa32270aa469e452d15477cebc61741f
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2026-02-16 23:12:34 -0300

    ref: Solve type warnings on centralized resolution module

--- src/gui/centralized_resolution.py
+++ src/gui/centralized_resolution.py
@@ -1,14 +1,17 @@
-from PyQt5.QtCore import QRect, QPoint
-from PyQt5.QtWidgets import QApplication
+
+from PyQt5.QtCore import QPoint, QRect
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
 class CentralizedAppResolution:
-    def __init__(self, app=None):
+    def __init__(self, app: QMainWindow):
         super(CentralizedAppResolution, self).__init__()
-        self.geometry: QRect = QApplication.primaryScreen().geometry()
+        application_primary_screen = QApplication.primaryScreen()
+        if application_primary_screen:
+            self.geometry: QRect = application_primary_screen.geometry()
         self.app = app
 
-    def centralized_resolution(self) -> QPoint | QPoint:
-        width = ((self.geometry.width() - self.app.width()) // 2)
-        height = ((self.geometry.height() - self.app.height()) // 2)
+    def centralized_resolution(self) -> QPoint:
+        width = (self.geometry.width() - self.app.width()) // 2
+        height = (self.geometry.height() - self.app.height()) // 2
         return QPoint(width, height)
