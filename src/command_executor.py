02464015a1d95511d97b0029cbae66e2ad58416e
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2026-02-27 21:26:01 -0300

    ref: CommandExecutor reworked to a stateless function command_executor

--- src/command_executor.py
+++ src/command_executor.py
@@ -4,26 +4,27 @@ import traceback
 import typing
 
 logger = logging.getLogger(__name__)
-class CommandExecutor:
-    def __init__(self, command: typing.Union[list[str], str], label_changer: typing.Optional[typing.Callable[[typing.Text], None]]=None):
-        self.command = command
-        self.label_changer = label_changer
 
-    def execute(self):
-        try:
-            subprocess.Popen(
-                args=self.__command_processor(self.command),
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL
-            )
-            logging.info(f"INFO - Executando {self.command}")
-        except FileNotFoundError as file_not_found_error:
-            message = f"Comando não encontrado: {file_not_found_error}"
-            logging.error(message)
-            if self.label_changer:
-                self.label_changer(message)
-        except Exception:
-            logging.exception(traceback.format_exc())
+
+def command_executor(
+    command: list[str] | str,
+    label_changer: typing.Callable[[str], None] | None = None,
+    *_: typing.Any,
+):
+    try:
+        subprocess.Popen(
+            args=__command_processor(command),
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+        logging.info(f"Command executed `{command}`")
+    except FileNotFoundError as file_not_found_error:
+        message = f"Command not found: {file_not_found_error}"
+        logging.error(message)
+        if label_changer:
+            label_changer(message)
+    except Exception:
+        logging.exception(traceback.format_exc())
 
 def __command_processor(
     command: list[str] | str,
