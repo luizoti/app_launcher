3bfe4126869b661a6594a0cde909b349b97e7d0a
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2026-02-27 21:29:29 -0300

    feat: Add a separator to context menu

--- src/gui/components/context_menu.py
+++ src/gui/components/context_menu.py
@@ -4,8 +4,5 @@ from PySide6.QtWidgets import QMenu, QWidget
 
 
 class ContextMenu(QMenu):
-
-    def __init__(self, parent: typing.Optional[QWidget]=None, *args: typing.Any, **kwargs: typing.Any):
-        super(ContextMenu, self).__init__(parent=parent, *args, **kwargs)
-        self.change_visibility_action = self.addAction("Hide/Show") # type: ignore
-        self.exit_action = self.addAction("Exit") # type: ignore
+        self.addSeparator()
+        self.exit_action = self.addAction("Exit")
