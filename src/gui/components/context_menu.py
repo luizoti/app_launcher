90aa169b7210b869793118ca4129f9b27e39f419
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2026-02-27 21:30:16 -0300

    feat: Use QCoreApplication instance to connect to quit event

--- src/gui/components/context_menu.py
+++ src/gui/components/context_menu.py
@@ -6,3 +6,4 @@ from PySide6.QtWidgets import QMenu, QWidget
 class ContextMenu(QMenu):
         self.addSeparator()
         self.exit_action = self.addAction("Exit")
+        q_app_instance = QCoreApplication.instance()
