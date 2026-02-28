ea5a2c2b98d5568e3f85abb48fe99ead4d0d5adf
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2026-02-27 21:23:30 -0300

    ref: Simplified ActionManager and signals changed to str

--- src/gui/action_manager.py
+++ src/gui/action_manager.py
@@ -3,21 +3,15 @@ import logging
 
 from PySide6.QtCore import Signal, Slot
 
-from src.enums import actions_map
-
 logger = logging.getLogger(__name__)
 
 
 class ActionManager:
-    actions = Signal(int)
+    actions = Signal(str)
 
-    @Slot(int, name="action_handler")
-    def action_handler(self, action_id: int) -> None:
-        method_name = actions_map.get(action_id)
-        if not method_name:
-            return
-        method = getattr(self, method_name, None)
+    @Slot(str, name="action_handler", result=None)
+    def action_handler(self, action_name: str) -> None:
+        method = getattr(self, action_name, None)
         if callable(method):
             method()
-            return
-        logger.error(f"Erro: Método {method_name} não encontrado em {self.__class__.__name__}")
+        return
