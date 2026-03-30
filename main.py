ac28cbe02361311845db4d272a549efc14f2a759
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2026-03-29 23:51:57 -0300

    ref: Add type hints and setup_logging to main

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import os
 import sys
-from logging import getLogger
+from logging import Logger, getLogger
 
 from PySide6.QtWidgets import QApplication
 
 from src.gui.app import AppMainWindow
-from src.instance import (check_pid_exist, get_current_pid, read_pid_file,
-                          write_pid_file)
+from src.instance import check_pid_exist, get_current_pid, read_pid_file, write_pid_file
+from src.log import setup_logging
 
-logger = getLogger(__name__)
-
+setup_logging()
+logger: Logger = getLogger(__name__)
 
 os.environ.setdefault("QT_QPA_PLATFORM", "xcb")
 
@@ -19,12 +19,14 @@ app = QApplication(sys.argv)
 # )  # Impede que o app feche quando a última janela for fechada
 
 if __name__ == "__main__":
-    loaded_pid = read_pid_file()
+    loaded_pid: int = read_pid_file()
     if loaded_pid:
         if check_pid_exist(loaded_pid):
-            logger.debug("Another instancie running with PID:", loaded_pid)
+            logger.debug(
+                f"Another instancie running with PID: {loaded_pid}",
+            )
             sys.exit(1)
-    logger.info("Started with PID:", get_current_pid())
+    logger.info(f"Started with PID: {get_current_pid()}")
     write_pid_file()
     app_window = AppMainWindow()
     app_window.show_ui()
