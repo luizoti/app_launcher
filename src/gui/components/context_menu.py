e23724bcd1d3883a5de1cb8b45923d9b90b03503
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2026-03-29 23:51:26 -0300

    ref: Add type hints to context_menu

--- src/gui/components/context_menu.py
+++ src/gui/components/context_menu.py
@@ -5,6 +5,14 @@ from PySide6.QtWidgets import QMenu, QWidget
 
 
 class ContextMenu(QMenu):
+    def __init__(
+        self,
+        parent: QWidget | None = None,
+        *args: typing.Any,
+        **kwargs: typing.Any,
+    ):
+        super().__init__(*args, **kwargs, parent=parent)
+        self.change_visibility_action = self.addAction("Hide/Show")
         self.addSeparator()
         self.exit_action = self.addAction("Exit")
         q_app_instance = QCoreApplication.instance()
