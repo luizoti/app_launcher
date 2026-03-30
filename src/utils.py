7029460ad09d3061712f291c0a1b4474da4681eb
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2026-03-29 23:52:06 -0300

    ref: Update check_running_processes signature

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,13 @@
-
 import psutil
 
 
-def check_running_processes(search_process: str):
-    if not search_process:
-        raise ValueError("Argument `search_process` cannot be None")
-
+def check_running_processes(
+    search_process: list[str],
+):
+    """TODO: Revise this function, this is not used, and the utility is not clear
+    anymore i don't if this is useful, but it can be used to check if a
+    process is running, and avoid opening the app if the process is already running.
+    """
     return (
         x
         for x in [x.info["name"].lower() for x in psutil.process_iter(["name"])]
