618d07d9e3d48b83134daa776fce9ea5f53b2051
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2026-03-29 23:41:37 -0300

    ref: Simplify AppGrid with type hints
    - Replace dict with list for mapped_grid
    - Add type hints to all methods and attributes
    - Use current_row/current_app instead of scanning for focus
    - Remove unused code and simplify navigation logic
    - Fix name mangling: __last_position to _last_position

--- src/gui/components/grid.py
+++ src/gui/components/grid.py
@@ -1,76 +1,35 @@
-import traceback
+import logging
 import typing
-from typing import Dict, Text
 
 import more_itertools
-from PySide6.QtCore import Slot
-from PySide6.QtWidgets import QGridLayout
+from PySide6.QtCore import Qt, Signal, Slot
+from PySide6.QtWidgets import QGridLayout, QWidget
 
-from src.command_executor import CommandExecutor
+from src import command_executor
 from src.gui.action_manager import ActionManager
 from src.gui.components.custom_button import CustomButton
 from src.settings_model import AppsModel
 
+LOGGER: logging.Logger = logging.getLogger(__name__)
+
 
 class AppGrid(QGridLayout, ActionManager):
+    actions = Signal(int)
 
     def __init__(self, row_limit: int = 6):
-        super(AppGrid, self).__init__()
-        self.row_limit = row_limit
-        # TODO: Now, this is a predefined fixed app row; the tuple represents this fixed size.
-        # In the future, if it is possible to add new apps, this can be changed to a list.
-        self.mapped_grid: dict[int, tuple[CustomButton, ...]] = {}
-
-        if not self.row_limit:
-            raise AttributeError
-
-    # def __remove_from_grid(self, app_name=None):
-    #     # TODO: This cam be changed, maybe search on self.mapped_grid
-    #     if not app_name:
-    #         raise TypeError("Argument `app_name` cannot be null")
-    #     index, app_widget = self.__search_app_on_grid(app_name=app_name)
-    #     widget = app_widget.widget()
-    #     if widget is not None:
-    #         self.removeWidget(widget)
-    #         widget.deleteLater()
-    #     # self.__reorganize_grid()
-    #     self.plot_app_grid()
-
-    # TODO: __rebuild_mapped_grid probably replace this
-    # def __reorganize_grid(self):
-    #     widgets = []
-    #     for i in reversed(range(self.count())):
-    #         item = self.takeAt(i)
-    #         widget = item.widget()
-    #         if widget:
-    #             widgets.append(widget)
-    #     row = 0
-    #     col = 0
-    #     for widget in reversed(widgets):
-    #         self.addWidget(widget, row, col)
-    #         col += 1
-    #         if col >= self.row_limit:
-    #             col = 0
-    #             row += 1
-
-    # def __search_app_on_grid(self, app_name):
-    #     for app_index in range(len(self.mapped_grid)):
-    #         button = self.itemAt(app_index)
-    #         print("button", button)
-    #         try:
-    #             widget:QWidget = button.widget()
-    #             if not widget:
-    #                 raise Exception("Widget of button cannot be None")
-    #             if widget.name == app_name:
-    #                 return app_index, button
-    #         except Exception as err:
-    #             raise err
-    #     return None
+        super().__init__()
+        self.row_limit: int = row_limit
+        self.mapped_grid: list[tuple[CustomButton, ...]] = []
+        self._last_position: tuple[int, ...] = (0, 0)
+        self.current_row: int = 0
+        self.current_app: int = 0
 
     @staticmethod
-    def __button_generator(app_name: typing.Text, app_data:AppsModel, label_changer: typing.Optional[typing.Callable[[typing.Text], None]]=None):
-        if not label_changer:
-            raise TypeError("Argument `label_changer` cannot be null")
+    def __button_generator(
+        app_name: str,
+        app_data: AppsModel,
+        label_changer: typing.Callable[[str], None],
+    ) -> CustomButton:
         """Create a button with command based on app name and app data."""
         return CustomButton(
             icon=app_data.icon,
@@ -80,120 +39,95 @@ class AppGrid(QGridLayout, ActionManager):
             name=app_name,
         )
 
-    def __rebuild_mapped_grid(self, apps: Dict[Text, AppsModel], label_changer: typing.Optional[typing.Callable[[typing.Text], None]]=None) -> None:
+    def __rebuild_mapped_grid(
+        self,
+        apps: dict[str, AppsModel],
+        label_changer: typing.Callable[[str], None],
+    ) -> None:
         """Rebuild mapped grid dictionary based on settings app list."""
-        if not apps:
-            raise TypeError("Argument `apps` cannot be None")
-        apps_iter: typing.Generator[CustomButton] = (self.__button_generator(*x, label_changer=label_changer) for x in apps.items())
-        self.mapped_grid = dict(enumerate(
-                more_itertools.batched(
-                    apps_iter,
-                    self.row_limit,
-                )
-            ))
+        apps_iter: typing.Generator[CustomButton] = (
+            self.__button_generator(*x, label_changer=label_changer)
+            for x in apps.items()
+        )
+        self.mapped_grid: list[tuple[CustomButton, ...]] = list(
+            more_itertools.batched(
+                iterable=apps_iter,
+                n=self.row_limit,
+            )
+        )
+        del apps_iter
 
-    def enter(self):
-        """Emit clicked signal from CustomButton to opem app and hide AppMainWindow based on app row and app columns."""
-        row_index, app_index = self.__get_current_focus()
-        pressed_button: CustomButton = self.mapped_grid[row_index][app_index]
-        pressed_button.clicked.emit()
-        parent_widget = self.parentWidget()
+    def enter(self) -> None:
+        """
+        Emit clicked signal from CustomButton to opem app and hide AppMainWindow
+        based on app row and app columns.
+        """
+        self.mapped_grid[self.current_row][self.current_app].clicked.emit()
+        parent_widget: QWidget = self.parentWidget()
         if parent_widget:
-            sub_parent_widget = parent_widget.parentWidget()
+            sub_parent_widget: QWidget | None = parent_widget.parentWidget()
             if sub_parent_widget:
                 sub_parent_widget.hide()
 
-    @Slot(str)
-    def _change_focus_on_hover(self, focused_app: typing.Text) -> None:
-        for row_index, apps in self.mapped_grid.items():
-            for app_index, app in enumerate(apps):
-                if focused_app == app.name:
-                    self.__set_focus(row_index, app_index)
+    @Slot()
+    def _change_focus_on_hover(self) -> None:
+        print("focused_app", self.current_row, self.current_app)
+        self.__set_focus(row_index=self.current_row, app_index=self.current_app)
 
-    def plot_app_grid(self, apps: Dict[Text, AppsModel], label_changer:typing.Optional[typing.Callable[[typing.Text], None]]=None) -> typing.Self:
+    def plot_app_grid(
+        self,
+        apps: dict[str, AppsModel],
+        label_changer: typing.Callable[[str], None],
+    ) -> "AppGrid":
         """Feed GridWidgets based on a list of apps."""
-        if not apps:
-            raise TypeError("Argument `apps` cannot be None")
         self.__rebuild_mapped_grid(apps=apps, label_changer=label_changer)
-        for row_index, apps_tuple in self.mapped_grid.items():
-            for app_index, app in enumerate(apps_tuple):
-                if label_changer:
-                    app.focused.connect(label_changer)
-                app.focused.connect(self._change_focus_on_hover)
-                self.addWidget(app, row_index, app_index, Qt.AlignmentFlag.AlignCenter)
+        for row_index, apps_tuple in enumerate(iterable=self.mapped_grid):
+            for app_index, app in enumerate(iterable=apps_tuple):
+                app.focused_change_label.connect(label_changer)
+                app.change_focus_on_hover.connect(self._change_focus_on_hover)
+                self.addWidget(
+                    app, row_index, app_index, alignment=Qt.AlignmentFlag.AlignCenter
+                )
         return self
-
-    def __get_current_focus(self) -> tuple[int, int]:
-        for row_index, apps in self.mapped_grid.items():
-            for app_index, button in enumerate(apps):
-                if button.hasFocus():
-                    return row_index, app_index
-        return -1, -1
 
     def __set_focus(self, row_index: int, app_index: int) -> None:
         try:
-            list(self.mapped_grid.values())[row_index][app_index].setFocus()
+            self.mapped_grid[row_index][app_index].setFocus()
+            self._last_position = (self.current_row, self.current_app)
         except IndexError:
-            print("Index out of range {}, {}".format(row_index, app_index))
+            self.__set_focus(*self._last_position)
+            print("ERROR")
         except Exception:
-            print(traceback.format_exc())
-
-    def up(self):
-        row_index, app_index = self.__get_current_focus()
-        if row_index == 0:
-            if (
-                app_index > len(list(self.mapped_grid.values())[-1]) - 1
-                and len(self.mapped_grid.values()) > 2
-            ):
-                self.__set_focus(-2, app_index)
-                return
-            if app_index > len(list(self.mapped_grid.values())[-1]) - 1:
-                print(app_index, len(list(self.mapped_grid.values())[-1]) - 1)
-                self.__set_focus(-1, len(list(self.mapped_grid.values())[-1]) - 1)
-                return
-            if app_index >= len(list(self.mapped_grid.values())[-1]) - 1:
-                self.__set_focus(-1, app_index)
-                return
-            else:
-                self.__set_focus(-1, app_index)
-                return
-        self.__set_focus(row_index - 1, app_index)
-        return
-
-    def down(self):
-        row_index, app_index = self.__get_current_focus()
-        if app_index > len(list(self.mapped_grid.values())[-1]) - 1 and row_index >= 1:
-            self.__set_focus(0, app_index)
-            return
-        if app_index > len(list(self.mapped_grid.values())[-1]) - 1:
-            self.__set_focus(row_index + 1, app_index)
-            return
-        if row_index == len(self.mapped_grid) - 1:
-            self.__set_focus(0, app_index)
-            return
-        self.__set_focus(row_index + 1, app_index)
-        return
-
-    def left(self):
-        row_index, app_index = self.__get_current_focus()
-        if app_index == 0:
-            self.__set_focus(
-                row_index - 1, len(list(self.mapped_grid.values())[row_index - 1]) - 1
+            LOGGER.exception(
+                f"Error on __set_focus(row_index={row_index}, app_index={app_index})"
             )
-            return
-        self.__set_focus(row_index, app_index - 1)
-        return
 
-    def right(self):
-        row_index, app_index = self.__get_current_focus()
-        if (
-            row_index == len(self.mapped_grid) - 1
-            and app_index == len(list(self.mapped_grid.values())[row_index]) - 1
-        ):
-            self.__set_focus(0, 0)
-            return
-        if app_index == self.row_limit - 1:
-            self.__set_focus(row_index + 1, 0)
-            return
-        self.__set_focus(row_index, app_index + 1)
-        return
+    def up(self) -> None:
+        print("up")
+        if self.current_row > 0:
+            self.current_row -= 1
+        else:
+            self.current_row += 1
+        self.__set_focus(self.current_row, self.current_app)
+
+    def down(self) -> None:
+        print("down")
+        if self.current_row > 0:
+            self.current_row -= 1
+        else:
+            self.current_row += 1
+        self.__set_focus(self.current_row, self.current_app)
+
+    def left(self) -> None:
+        print("left")
+        if self.current_app > 0:
+            self.current_app -= 1
+        else:
+            self.current_app = self.row_limit - 1
+            self.current_row += 1
+        self.__set_focus(self.current_row, self.current_app)
+
+    def right(self) -> None:
+        print("right")
+        self.current_app += 1
+        self.__set_focus(self.current_row, self.current_app)
