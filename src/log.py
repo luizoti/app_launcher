119b1d7145419e71c1dd491d26c1ed2e3b1d4934
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2026-04-14 10:21:51 -0300

    fix: Change log path to settings directory

--- src/log.py
+++ src/log.py
@@ -1,19 +1,21 @@
 import logging
-import os
 from logging.handlers import RotatingFileHandler
+from typing import TextIO
+
+from src.settings import CONFIG_DIRECTORY
 
 
 def setup_logging():
     log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
-    root_logger = logging.getLogger()
+    root_logger: logging.Logger = logging.getLogger()
     root_logger.setLevel(logging.INFO)
-    log_path = os.path.join(os.getcwd(), "app_launcher.log")
+    log_path = CONFIG_DIRECTORY.joinpath("app_launcher.log")
     file_handler = RotatingFileHandler(
         log_path, maxBytes=1024 * 1024 * 2, backupCount=5
     )
     file_handler.setFormatter(logging.Formatter(log_format))
-    console_handler = logging.StreamHandler()
+    console_handler: logging.StreamHandler[TextIO] = logging.StreamHandler()
     console_handler.setFormatter(logging.Formatter(log_format))
     root_logger.addHandler(file_handler)
     root_logger.addHandler(console_handler)
