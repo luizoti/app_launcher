645006e9171bf2942bff65cbf7b04b944e3e439c
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2026-07-02 22:49:41 -0300

    test(utils): add unit tests for cmdline and substring matching

--- tests/test_check_running_process.py
+++ tests/test_check_running_process.py
@@ -20,7 +20,7 @@ class TestCheckRunningProcess(unittest.TestCase):
         result = list(
             check_running_processes(search_process=["kodi", "emulationstation"])
         )
-        self.assertEqual(result, ["kodi", "emulationstation"])
+        self.assertCountEqual(result, ["kodi", "emulationstation"])
 
     @patch("src.utils.psutil.process_iter")
     def test_returns_empty_when_none_match(self, mock_process_iter):
@@ -40,7 +40,7 @@ class TestCheckRunningProcess(unittest.TestCase):
         result = list(
             check_running_processes(search_process=["kodi", "emulationstation"])
         )
-        self.assertEqual(result, ["kodi", "emulationstation"])
+        self.assertCountEqual(result, ["kodi", "emulationstation"])
 
     @patch("src.utils.psutil.process_iter")
     def test_case_insensitive_search(self, mock_process_iter):
@@ -49,6 +49,42 @@ class TestCheckRunningProcess(unittest.TestCase):
         ]
         result = list(check_running_processes(search_process=["Kodi"]))
         self.assertEqual(result, ["Kodi"])
+
+    @patch("src.utils.psutil.process_iter")
+    def test_matches_via_cmdline_wrapper(self, mock_process_iter):
+        mock_process_iter.return_value = [
+            self._make_process(
+                "x-terminal-emulator",
+                cmdline=["x-terminal-emulator", "-e", "emulationstation"],
+            ),
+        ]
+        result = check_running_processes(search_process=["emulationstation"])
+        self.assertEqual(result, ["emulationstation"])
+
+    @patch("src.utils.psutil.process_iter")
+    def test_matches_substring_in_cmdline(self, mock_process_iter):
+        mock_process_iter.return_value = [
+            self._make_process("moonlight-qt", cmdline=["moonlight-qt"]),
+        ]
+        result = check_running_processes(search_process=["moonlight"])
+        self.assertEqual(result, ["moonlight"])
+
+    @patch("src.utils.psutil.process_iter")
+    def test_deduplicates_matches(self, mock_process_iter):
+        mock_process_iter.return_value = [
+            self._make_process("kodi", cmdline=["kodi"]),
+            self._make_process("firefox", cmdline=["kodi-wayland"]),
+        ]
+        result = check_running_processes(search_process=["kodi"])
+        self.assertEqual(result, ["kodi"])
+
+    @patch("src.utils.psutil.process_iter")
+    def test_no_match_when_term_absent(self, mock_process_iter):
+        mock_process_iter.return_value = [
+            self._make_process("firefox", cmdline=["firefox", "https://x.com"]),
+        ]
+        result = check_running_processes(search_process=["kodi", "emulationstation"])
+        self.assertEqual(result, [])
 
 
 if __name__ == "__main__":
