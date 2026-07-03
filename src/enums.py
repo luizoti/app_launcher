4defd9ab6575ee6d2b9430320e998c8f4dc412aa
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2026-07-02 22:02:03 -0300

    chore: remove dead enums.py with unused actions_map

--- src/enums.py
+++ src/enums.py
@@ -1,13 +1,1 @@
 
-actions_map: dict[int, str] = {
-    1: "up",
-    2: "down",
-    3: "left",
-    4: "right",
-    5: "enter",
-    6: "options",
-    7: "toggle_view",
-    8: "close",
-}
-
-actions_map_reversed: dict[str, int] = {y: x for x, y in actions_map.items()}
